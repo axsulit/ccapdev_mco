d460a832c19e272ec004d896bca686ba0eb190ca
ysobella <ysobella03@gmail.com>
2023-05-30 19:43:02 +0800

    updated home page and login

---
+++ javascript/login-script.js
@@ -0,0 +1,8 @@
+let loginButton = document.getElementById('login');
+
+// Add a click event listener to the button
+loginButton.addEventListener('click', () => {
+  window.location.href = "index.html";
+})
+
+
