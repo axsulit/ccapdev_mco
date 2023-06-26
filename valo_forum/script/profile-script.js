a5c1ba4f175a749f8591ce98fef7d87918c4d56f
ysobella <ysobella03@gmail.com>
2023-06-26 23:20:30 +0800

    edit profile layou

---
+++ valo_forum/script/profile-script.js
@@ -0,0 +1,11 @@
+/*** POPUP FUNCTIONS ***/
+
+function showPopup() {
+    var popupContainer = document.getElementById("edit-profile-container");
+    popupContainer.style.display = "block";
+}
+
+function closePopup() {
+    var popupContainer = document.getElementById("edit-profile-container");
+    popupContainer.style.display = "none";
+}
