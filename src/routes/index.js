2dd3c64b6e1383aa9e27fb9d527658cce4685337
vmarissaann <marissa_villaceran@dlsu.edu.ph>
2023-07-20 20:39:14 +0800

    navbar

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,7 @@ import {Router} from 'express';
 const router=Router();
 
 router.get("/",(req, res)=>{
-    res.render("partials/navbar",{
+    res.render("layouts/main",{
         title: "Homepage"
     });
 });
