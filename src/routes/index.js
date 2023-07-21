3cc5cd0b94f3744c50983a63f44a6c70cc1ac904
vmarissaann <marissa_villaceran@dlsu.edu.ph>
2023-07-21 08:33:40 +0800

    redirect to /profile

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,15 +3,17 @@ import {Router} from 'express';
 const router=Router();
 
 router.get("/",(req, res)=>{
-    // res.render("layouts/main",{
-    //     title: "Homepage"
-    // });
-    // res.send("hello World!");
-   // res.render("partials/test",{test: "/views/main"});
+    
    res.render("index", {
     title: "Homepage"
     });
   
 });
+
+router.get("/profile",(req, res)=>{
+    
+    res.send("Not yet done but working");
+   
+ });
 
 export default router;
