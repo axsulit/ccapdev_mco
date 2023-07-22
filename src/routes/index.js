416bc487b57058c242f56ad3d8dee9fd6283b5fc
vmarissaann <marissa_villaceran@dlsu.edu.ph>
2023-07-22 14:07:12 +0800

    retrieving user posts

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,18 @@
 import {Router} from 'express';
 import userRouter from './userRouter.js';
+import { getDb } from '../db/conn.js';
 
+const db=getDb();
+const posts = db.collection("posts");
 
 const router=Router();
 
 
 router.get("/",async (req, res)=>{
-   res.render("index", {
-    title: "Homepage",
+    const postsArray = await posts.find({}).toArray();
+    res.render("index", {
+        title: "Homepage",
+        posts: postsArray
     });
   
 });
