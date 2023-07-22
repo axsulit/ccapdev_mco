17536372f92751f15d04939f07f6d9801d0079fb
vmarissaann <marissa_villaceran@dlsu.edu.ph>
2023-07-23 06:06:43 +0800

    change username on profile depends on user

--- src/routes/profileRouter.js
+++ src/routes/profileRouter.js
@@ -3,13 +3,33 @@ import { getDb } from '../db/conn.js';
 
 const profileRouter=Router();
 const db=getDb();
-//const users = db.collection("users");
+const users = db.collection("users");
 
 //TO DO: profile with userID
-profileRouter.get("/profile/:username",(req, res)=>{ 
-    res.render("profile", {
-        title: "Profile",
+profileRouter.get("/profile/:username", async (req, res)=>{ 
+    const param_username=req.params.username;
+    //console.log("username",param_username);
+
+    // const user = await users.findOne({ 
+    //     username: req.body.username
+    // });
+    const usersArray = await users.find({}).toArray();
+    //console.log(usersArray);
+    const user = await users.findOne({ 
+        username: req.params.username,  
     });
+    if(user){
+        res.render("profile", {
+            title: "Profile",
+            username:user.username
+        });
+    }
+    else{
+        res.render("error", {
+            title: "Page not Found."
+        });
+    }
+   
  });
  
 
