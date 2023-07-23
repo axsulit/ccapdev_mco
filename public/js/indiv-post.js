a7f816c410b0c782bc6f66c6845eb865767e5e07
vmarissaann <marissa_villaceran@dlsu.edu.ph>
2023-07-24 06:51:03 +0800

    edited content

--- public/js/indiv-post.js
+++ public/js/indiv-post.js
@@ -9,14 +9,11 @@ const saveButton=document.querySelector('.save-button');
     contentEdit.style.display = 'block';
     saveButton.style.display = 'block';
     contentEdit.focus();
+    
   });
 
  saveButton?.addEventListener('click', async (e) => {
     e.preventDefault();
-
-    // contentText.style.display = 'block';
-    // contentEdit.style.display = 'none';
-    // saveButton.style.display = 'none';
     contentEdit = document.querySelector(".content-edit");
     postID=document.querySelector("#postid").textContent;
     console.log(postID);
@@ -43,6 +40,9 @@ const saveButton=document.querySelector('.save-button');
         });
         console.log(response);
         if (response.status === 200) {
+            contentText.style.display = 'block';
+            contentEdit.style.display = 'none';
+            saveButton.style.display = 'none';
            location.reload();
         } else {
             console.log("Status code received: " + response.status);
@@ -51,5 +51,8 @@ const saveButton=document.querySelector('.save-button');
     } catch (err) {
             console.error('Error occurred:', err);
     } 
+
+    
+   
     
   });
