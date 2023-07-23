cf066adbbead1f50cbd40ad3f3756a015690b2f0
vmarissaann <marissa_villaceran@dlsu.edu.ph>
2023-07-24 05:56:53 +0800

    edit buttons

--- public/js/indiv-post.js
+++ public/js/indiv-post.js
@@ -1,1 +1,19 @@
 //bella work here
+const editButton = document.querySelector('.edit-button');
+const contentText = document.querySelector('.content-text');
+const contentEdit = document.querySelector('.content-edit');
+const saveButton=document.querySelector('.save-button');
+
+  editButton.addEventListener('click', () => {
+    contentText.style.display = 'none';
+    contentEdit.style.display = 'block';
+    saveButton.style.display = 'block';
+    contentEdit.focus();
+  });
+
+ saveButton.addEventListener('click', () => {
+    contentText.style.display = 'block';
+    contentEdit.style.display = 'none';
+    saveButton.style.display = 'none';
+    
+  });
