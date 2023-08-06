e8e7265bd7ed120c383528f7ecd6cb77dde17b5e
zhoexaeris <zhoe_gongon@dlsu.edu.ph>
2023-08-06 14:01:38 +0800

    open and close edit comments

--- public/js/comment.js
+++ public/js/comment.js
@@ -1,5 +1,17 @@
-const writeEditComment = document.querySelector(".edit-comment-border");
-const editComment = document.querySelector(".commentEdit-btn");
-const exitComment = document.querySelector(".close-post");
+// get all elements with the class name "edit-comment-border"
+const allOpenEditComments = document.querySelectorAll(".edit-comment-border");
+const editCommentBtns = document.querySelectorAll(".commentEdit-btn");
+const closeCommentBtns = document.querySelectorAll(".close-edit-comment");
 
-editComment.addEventListener("click", () => writeEditComment.classList.toggle("active"));
+// iterate through each comment element and attach the event listener
+for (let i = 0; i < editCommentBtns.length; i++) {
+  const editCommentBtn = editCommentBtns[i];
+  const openEditComment = allOpenEditComments[i];
+  const closeEditComment = closeCommentBtns[i];
+
+  // opens edit comment
+  editCommentBtn.addEventListener("click", () => { openEditComment.classList.toggle("active");});
+  // closes edit comment
+  closeEditComment.addEventListener("click", () => openEditComment.classList.remove("active"));
+
+}
